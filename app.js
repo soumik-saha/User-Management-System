c24f3e754e67e1e7c41cccee8a93a0302ecd89cb
Soumik Saha <sahasoumik1573@gmail.com>
2023-07-12 17:53:30 +0530

    creating homepage ui

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Parsing middleware
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Parse application/json
+app.use(bodyParser.json());
+
+// Access all static files
+app.use(express.static('public'))  
+
+// Templating Engine
+app.engine('hbs', exphbs.engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+
+// Router
+app.get('/', (req, res) => {
+    res.render('home.hbs');
+});
+
+
+app.listen(port, () => console.log('Listening on port', port));
